639379c0f46656a8a5f578f9b55840866dd5cc92
Just-Joe-Barnes <jobybarnes1@yahoo.co.uk>
2025-02-21 19:29:04 +0000

    added css mornalize into index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,11 @@
+import 'normalize.css'; // Import normalize.css for consistent styling across browsers
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Adjust import
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
