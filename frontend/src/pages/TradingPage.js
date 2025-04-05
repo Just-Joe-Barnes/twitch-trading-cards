9b7597d25e67b78b9a2164b203cbada799de646a
Just-Joe-Barnes <jobybarnes1@yahoo.co.uk>
2025-04-05 20:45:11 +0100

    Style: add modern CSS for Trading Dashboard 11

--- frontend/src/pages/TradingPage.js
+++ frontend/src/pages/TradingPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { createTrade, searchUsers, fetchWithAuth } from "../utils/api";
 import BaseCard from "../components/BaseCard";
+import '../styles/TradingDashboard.css';
 // import "../styles/TradingPage.css";
 import { rarities } from "../constants/rarities";
 
