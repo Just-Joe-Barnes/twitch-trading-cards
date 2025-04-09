d333f3232d82c2b196a6059f0cba34def6f4dd7b
Just-Joe-Barnes <jobybarnes1@yahoo.co.uk>
2025-04-09 19:13:19 +0100

    Custom pack creation 1

--- backend/src/models/packModel.js
+++ backend/src/models/packModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Define the schema for packs
 const packSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Owner of the pack
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false }, // Owner of the pack (optional for templates)
     isOpened: { type: Boolean, default: false }, // Whether the pack has been opened
     openedAt: { type: Date }, // Timestamp for when the pack was opened
     cards: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Card' }], // Cards in the pack
